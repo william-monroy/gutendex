4538ecb735b613694a2a893c985ca49576635836
Santi Ayechu <santiayechu@gmail.com>
2024-04-29 21:53:42 +0200

    refactor: 💡 Añadir librerias y estructura basica App.tsx

--- trabajo-final/src/App.tsx
+++ trabajo-final/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App: React.FC = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      {/* <Header /> */}
+      <main className="px-responsive">
+        <h1>App</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          tenetur quaerat pariatur necessitatibus, aperiam exercitationem. Ab
+          enim placeat voluptate, at corrupti necessitatibus reprehenderit
+          tenetur officia! Dolores, similique consequuntur! Odit autem ipsum
+          deserunt adipisci itaque repudiandae distinctio magni nam cumque,
+          saepe laboriosam facere nemo deleniti a modi eveniet enim officiis
+          minima voluptatem cum sequi necessitatibus. Eos repudiandae in ipsam
+          aliquam impedit?
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {/* <Header /> */}
+        {/* <Hero /> */}
+        {/* <Features /> */}
+        {/* <Customers /> */}
+        {/* <Integrations /> */}
+      </main>
+      {/* <Footer /> */}
     </>
-  )
-}
-
-export default App
+  );
+};
--- trabajo-final/src/main.tsx
+++ trabajo-final/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import "./index.css";
+import { App } from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="light">
+        <App />
+      </NextThemesProvider>
+    </NextUIProvider>
+  </React.StrictMode>
+);
