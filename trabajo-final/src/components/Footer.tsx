2a0284f30c691cee7663a0d30ec381fb0759fb1a
Santi Ayechu <santiayechu@gmail.com>
2024-04-29 23:44:44 +0200

    refactor: 💡 Quitar código redundante

--- trabajo-final/src/components/Footer.tsx
+++ trabajo-final/src/components/Footer.tsx
@@ -1,15 +1,6 @@
 import React from "react";
-import { useEffect, useState } from "react";
 
 export const Footer: React.FC = () => {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
   return (
     <footer className="py-4 px-6 flex justify-between items-center mx-auto max-w-screen-lg flex-wrap">
       <a href="/" className="font-bold text-inherit">
