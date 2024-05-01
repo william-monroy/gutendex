af6039342b53aab02330241a8199e5a497cfa71b
Santi Ayechu <santiayechu@gmail.com>
2024-05-01 11:49:03 +0200

    refactor: 💡 Dividir componente Icono en secciones
    Se ha dividido la Card en Card Body y Card Footer; Además, se ha añadido
    atributo isHoverable

--- trabajo-final/src/components/Icono.tsx
+++ trabajo-final/src/components/Icono.tsx
@@ -1,14 +1,20 @@
-import { Card } from "@nextui-org/react";
+import { Card, CardBody, CardFooter } from "@nextui-org/react";
 import { Avatar } from "@nextui-org/react";
 
 export const Icono: React.FC = (props) => {
   return (
-    <Card className="flex flex-col items-center justify-center w-[200px] space-y-5 p-4 rounded-2xl">
-      <Avatar src={props.src} className="w-20 h-20 text-large" />
-      <div className="text-center space-y-3">
-        <h1 className="font-bold text-2xl">{props.heading}</h1>
-        <p>{props.paragraph}</p>
-      </div>
-    </Card>
+    <div className="flex justify-center items-center">
+      <Card isHoverable className="w-[200px] p-4 rounded-2xl">
+        <CardBody className="items-center">
+          <Avatar src={props.src} className="w-20 h-20 text-large" />
+        </CardBody>
+        <CardFooter className="text-center">
+          <div>
+            <h1 className="font-bold text-2xl">{props.heading}</h1>
+            <p>{props.paragraph}</p>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 };
