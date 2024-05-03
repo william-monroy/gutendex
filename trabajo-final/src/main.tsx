8cf64c5192d7199aca970bd3f3bcdc21fe8a399d
William Frank Monroy Mamani <william_monroy777@hotmail.com>
2024-05-03 13:13:17 +0200

    feat: 🎸 Implementacion de react-router-dom v6
    Se elimino <App /> y se utilizo el provider para crear un contexto
    global, se hizo la definicion de rutas en el archivo routes.tsx
    
    ✅ Closes: #9

--- trabajo-final/src/main.tsx
+++ trabajo-final/src/main.tsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import "./index.css";
-import { App } from "./App";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./config/routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="light">
-        <App />
+        <RouterProvider router={router} />
       </NextThemesProvider>
     </NextUIProvider>
   </React.StrictMode>
