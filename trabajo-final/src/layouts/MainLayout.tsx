25a0ddff4d59e29708151456a8f1a58186611910
William Frank Monroy Mamani <william_monroy777@hotmail.com>
2024-05-03 13:09:01 +0200

    refactor: 💡 Se refactorizo el Header main y Footer en un layout

---
+++ trabajo-final/src/layouts/MainLayout.tsx
@@ -0,0 +1,18 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+interface MainLayoutProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
+export const MainLayout: React.FC<MainLayoutProps> = ({
+  children,
+}: MainLayoutProps) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
