3759684ecc0c65b1338ab4400d8b747e50cb2812
Santi Ayechu <santiayechu@gmail.com>
2024-05-07 09:12:20 +0200

    refactor: 💡 Añadir instancias MiembroEquipo desde página About

--- trabajo-final/src/pages/About.tsx
+++ trabajo-final/src/pages/About.tsx
@@ -1,9 +1,39 @@
 import { MainLayout } from "@/layouts/MainLayout";
+import { MiembroEquipo } from "@components/MiembroEquipo";
 
 export const About: React.FC = () => {
   return (
     <MainLayout>
-      <h1>About</h1>
+      <div className="px-responsive flex flex-row flex-wrap justify-center">
+        <MiembroEquipo
+          src="https://media.discordapp.net/attachments/702600003998449805/1237297382585274388/william-animoji.png?ex=663b227b&is=6639d0fb&hm=44036546caf7c001ac1fd837d652a410633aa40df8f0df349de7fab22263691f&=&format=webp&quality=lossless&width=448&height=372"
+          width={250}
+          name="William Monroy"
+          role="Role"
+          className="m-1 max-w-44"
+        />
+        <MiembroEquipo
+          src="https://media.discordapp.net/attachments/702600003998449805/1237297382585274388/william-animoji.png?ex=663b227b&is=6639d0fb&hm=44036546caf7c001ac1fd837d652a410633aa40df8f0df349de7fab22263691f&=&format=webp&quality=lossless&width=448&height=372"
+          width={250}
+          name="William Monroy"
+          role="Role"
+          className="m-1 max-w-44"
+        />
+        <MiembroEquipo
+          src="https://media.discordapp.net/attachments/702600003998449805/1237297382585274388/william-animoji.png?ex=663b227b&is=6639d0fb&hm=44036546caf7c001ac1fd837d652a410633aa40df8f0df349de7fab22263691f&=&format=webp&quality=lossless&width=448&height=372"
+          width={250}
+          name="William Monroy"
+          role="Role"
+          className="m-1 max-w-44"
+        />
+        <MiembroEquipo
+          src="https://media.discordapp.net/attachments/702600003998449805/1237297382585274388/william-animoji.png?ex=663b227b&is=6639d0fb&hm=44036546caf7c001ac1fd837d652a410633aa40df8f0df349de7fab22263691f&=&format=webp&quality=lossless&width=448&height=372"
+          width={250}
+          name="William Monroy"
+          role="Role"
+          className="m-1 max-w-44"
+        />
+      </div>
     </MainLayout>
   );
 };
