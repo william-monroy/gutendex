95980d381944d062bc5cc985638d5fa1783dd644
Miguel de la Ossa <delaossa.148235@e.unavarra.es>
2024-05-07 11:07:16 +0200

    feat: 🎸 Añadir componente formatos

---
+++ trabajo-final/src/components/Formato.tsx
@@ -0,0 +1,43 @@
+import { Section } from "@/layouts/Section";
+import { Icono } from "./Icono";
+
+export const Formato: React.FC = () => {
+  return (
+    <Section isDark={false}>
+      <h2 className="text-4xl font-bold">Formatos disponibles</h2>
+      <p className="text-[#4D4C4D] dark:text-[#C6C6C6]">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        tenetur quaerat pariatur necessitatibus, aperiam exercitationem. Ab enim
+        placeat voluptate, at corrupti necessitatibus reprehenderit tenetur
+        officia!
+      </p>  
+
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center items-center gap-4 mt-8" style={{ gridAutoFlow: 'dense' }}>
+        <Icono
+          src="/images/undraw_contact_us_15o2.svg"
+          heading="HTML"
+        />
+        <Icono
+          src="/images/undraw_contact_us_15o2.svg"
+          heading="EPUB"
+        />
+        <Icono
+          src="/images/undraw_contact_us_15o2.svg"
+          heading="Mobipocket Ebook"
+        />
+        <Icono
+          src="/images/undraw_contact_us_15o2.svg"
+          heading="RDF"
+        />
+        <Icono
+          src="/images/undraw_contact_us_15o2.svg"
+          heading="Octect Stream"
+        />
+        <Icono
+          src="/images/undraw_contact_us_15o2.svg"
+          heading="Solo Texto"
+        />
+      </div>
+    </Section>
+  );
+};
