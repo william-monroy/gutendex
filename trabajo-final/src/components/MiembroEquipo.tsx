62e48d0da0cc5f4ba346844ca34eede2087afe08
Santi Ayechu <santiayechu@gmail.com>
2024-05-07 02:08:28 +0200

    feat: 🎸 Crear componente MiembroEquipo (img, nombre y rol)

---
+++ trabajo-final/src/components/MiembroEquipo.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+
+interface MiembroEquipoProps {
+  src: string;
+  name: string;
+  role: string;
+  className?: string | boolean | undefined;
+}
+
+export const MiembroEquipo: React.FC<MiembroEquipoProps> = (
+  props: MiembroEquipoProps
+) => {
+  return (
+    <Card className="py-4">
+      <CardHeader className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          className="object-cover rounded-xl"
+          src={props.src}
+          isZoomed
+        />
+      </CardHeader>
+      <CardBody className="pb-0 pt-2 px-4 flex-col items-start">
+        <h4 className="font-bold text-large">{props.name}</h4>
+        <small className="text-default-500">{props.role}</small>
+      </CardBody>
+    </Card>
+  );
+};
