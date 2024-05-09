d5ea49c5ae87fdeaf14233be69b9c1eefc086b7f
Santi Ayechu <santiayechu@gmail.com>
2024-05-09 16:57:11 +0200

    refactor: 💡 Añadir section con instancia de componente Filtros

--- trabajo-final/src/pages/Catalog.tsx
+++ trabajo-final/src/pages/Catalog.tsx
@@ -1,13 +1,19 @@
 import { MainLayout } from "@/layouts/MainLayout";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { Filtros } from "@/components/Filtros";
 
 export const Catalog: React.FC = () => {
   return (
     <MainLayout>
-      <h1>Catalog</h1>
-      <Link to="/catalog/1">Book 1</Link>
-      <Outlet />
+      <section className="px-responsive">
+        <h1 className="text-4xl font-bold text-center mb-8 mt-6">
+          Catálogo de Libros
+        </h1>
+        <Filtros />
+        <Link to="/catalog/1">Book 1</Link>
+        <Outlet />
+      </section>
     </MainLayout>
   );
 };
