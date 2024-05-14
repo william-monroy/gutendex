c8ab3b2449093df50415bae45ca3b4c748b04780
Miguel de la Ossa <delaossa.148235@e.unavarra.es>
2024-05-14 17:52:28 +0200

    fix: 🐛 Eliminar caracter

--- trabajo-final/src/components/Contacto.tsx
+++ trabajo-final/src/components/Contacto.tsx
@@ -6,10 +6,7 @@ export const Contacto: React.FC = () => {
     <Section isDark>
       <h2 className="text-4xl font-bold">Contáctanos</h2>
       <p className="text-[#4D4C4D] dark:text-[#C6C6C6]">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        tenetur quaerat pariatur necessitatibus, aperiam exercitationem. Ab enim
-        placeat voluptate, at corrupti necessitatibus reprehenderit tenetur
-        officia!
+        Es posible contactarnos si tiene cualquier consulta que hacernos. Ingrese aquí su mensaje y le contestaremos lo más pronto posible.
       </p>
 
       <div className="flex justify-center mx-8 mt-8">
--- trabajo-final/src/components/Formato.tsx
+++ trabajo-final/src/components/Formato.tsx
@@ -4,12 +4,10 @@ import { Icono } from "./Icono";
 export const Formato: React.FC = () => {
   return (
     <Section isDark={false}>
-      <h2 className="text-4xl font-bold">Formatos disponibles</h2>
+      <h2 className="text-4xl font-bold">Formatos Disponibles</h2>
       <p className="text-[#4D4C4D] dark:text-[#C6C6C6]">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        tenetur quaerat pariatur necessitatibus, aperiam exercitationem. Ab enim
-        placeat voluptate, at corrupti necessitatibus reprehenderit tenetur
-        officia!
+        Descubre los diferentes formatos de libros disponibles en nuestra plataforma.
+        Conviértete en un lector ávido con la variedad de opciones que ofrecemos.
       </p>  
 
       <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center items-center gap-4 mt-8" style={{ gridAutoFlow: 'dense' }}>
@@ -20,7 +18,7 @@ export const Formato: React.FC = () => {
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
           heading="EPUB"
-        />z
+        />
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
           heading="Mobipocket Ebook"
