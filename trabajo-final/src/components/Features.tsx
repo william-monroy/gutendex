ac1e6fd68a82e95a517e3ae4b380cf448c7d5ace
Miguel de la Ossa <delaossa.148235@e.unavarra.es>
2024-05-14 17:42:10 +0200

    refactor: 💡 Añadido conntenido de texto en la Landing Page

--- trabajo-final/src/components/Features.tsx
+++ trabajo-final/src/components/Features.tsx
@@ -4,29 +4,27 @@ import { Icono } from "./Icono";
 export const Features: React.FC = () => {
   return (
     <Section isDark>
-      <h2 className="text-4xl font-bold">Lorem ipsum dolor</h2>
+      <h2 className="text-4xl font-bold">Tipos de Filtros</h2>
       <p className="text-[#4D4C4D] dark:text-[#C6C6C6]">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-        tenetur quaerat pariatur necessitatibus, aperiam exercitationem. Ab enim
-        placeat voluptate, at corrupti necessitatibus reprehenderit tenetur
-        officia!
+        Encuentra libros por autores, idiomas, categorías y más. Explora nuestra
+        colección y descubre nuevos títulos que te apasionen.
       </p>
 
       <div className="flex justify-center items-center flex-wrap gap-4 mt-8">
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
           heading="Autores"
-          paragraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          paragraph="Explora libros escritos por tus autores favoritos."
         />
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
           heading="Idioma"
-          paragraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          paragraph="Encuentra libros en tu idioma preferido."
         />
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
-          heading="Categoria"
-          paragraph="Lorem, ipsum dolor sit amet consectetur adipisicing elit."
+          heading="Categoría"
+          paragraph="Explora libros por género y temas específicos."
         />
       </div>
     </Section>
