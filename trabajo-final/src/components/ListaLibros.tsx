6813601836e2f4c6a70e625aa08dd4126d0ebafe
Santi Ayechu <santiayechu@gmail.com>
2024-05-14 18:06:19 +0200

    fix: 🐛 Eliminar componente de Filtros y añadir en ListaLibros

--- trabajo-final/src/components/ListaLibros.tsx
+++ trabajo-final/src/components/ListaLibros.tsx
@@ -4,6 +4,17 @@ import { Libro } from "@/components/Libro";
 import LibroLoading from "./LibroLoading";
 import useBooks from "@/hooks/useBooks";
 import { useQueryParam } from "@/hooks/useQueryParam";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Input,
+} from "@nextui-org/react";
+import { DropdownIcon } from "@/icons/DropdownIcon";
+import { SearchIcon } from "@/icons/SearchIcon";
+import { useMemo, useState } from "react";
 
 interface Book {
   id: number;
@@ -23,6 +34,20 @@ function ListaLibros() {
 
   const navigate = useNavigate();
 
+  const [selectedKeys, setSelectedKeys] = useState(
+    new Set(["Selecciona un filtro"])
+  );
+  const [searchText, setSearchText] = useState("");
+
+  const selectedValue = useMemo(
+    () =>
+      Array.from(selectedKeys)
+        .join(", ")
+        .replace("Selecciona un filtro,", "")
+        .replaceAll("_", " "),
+    [selectedKeys]
+  );
+
   if (isLoading)
     return (
       <div className="flex flex-wrap justify-center gap-4">
@@ -40,37 +65,81 @@ function ListaLibros() {
     );
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {data?.results?.map((libro: Book) => (
-        <Link
-          to={`/catalog/${libro.id}?page=${query.get("page") ?? "1"}`}
-          key={libro.id}
-        >
-          <Libro
-            src={libro.formats["image/jpeg"]}
-            titulo={libro.title}
-            autor={libro.authors
-              .map((author) => {
-                const nameParts = author.name.split(",");
-                return `${nameParts.slice(1).join("")} ${nameParts[0]}`;
-              })
-              .join(", ")}
-          />
-        </Link>
-      ))}
-      {data?.results?.length > 0 && (
-        <Pagination
-          isCompact
-          showControls
-          total={Math.ceil(data?.count / 32)}
-          initialPage={
-            query.get("page") ? parseInt(query.get("page") || "1") : 1
-          }
-          onChange={(page) => {
-            navigate(`/catalog?page=${page}`);
-          }}
+    <div>
+      <div className="flex justify-center items-center flex-wrap gap-4 mb-8">
+        <h3 className="font-bold">Buscar por:</h3>
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant="bordered" endContent={<DropdownIcon />}>
+              {selectedValue || "Selecciona un filtro"}
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu
+            variant="flat"
+            aria-label="Multiple selection example"
+            closeOnSelect={false}
+            disallowEmptySelection
+            selectionMode="single"
+            selectedKeys={selectedKeys}
+            onSelectionChange={(keys) => setSelectedKeys(keys as Set<string>)}
+          >
+            <DropdownItem key="idioma">Idioma</DropdownItem>
+            <DropdownItem key="tema">Tema</DropdownItem>
+            <DropdownItem key="edit">Edit</DropdownItem>
+            <DropdownItem key="delete">Delete file</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        <Input
+          isClearable
+          type="text"
+          variant="bordered"
+          placeholder="Introduce el término de búsqueda"
+          className="max-w-xs"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
         />
-      )}
+
+        <Button
+          color="primary"
+          startContent={<SearchIcon />}
+          isDisabled={!searchText}
+        >
+          Buscar
+        </Button>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4">
+        {data?.results?.map((libro: Book) => (
+          <Link
+            to={`/catalog/${libro.id}?page=${query.get("page") ?? "1"}`}
+            key={libro.id}
+          >
+            <Libro
+              src={libro.formats["image/jpeg"]}
+              titulo={libro.title}
+              autor={libro.authors
+                .map((author) => {
+                  const nameParts = author.name.split(",");
+                  return `${nameParts.slice(1).join("")} ${nameParts[0]}`;
+                })
+                .join(", ")}
+            />
+          </Link>
+        ))}
+        {data?.results?.length > 0 && (
+          <Pagination
+            isCompact
+            showControls
+            total={Math.ceil(data?.count / 32)}
+            initialPage={
+              query.get("page") ? parseInt(query.get("page") || "1") : 1
+            }
+            onChange={(page) => {
+              navigate(`/catalog?page=${page}`);
+            }}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- trabajo-final/src/pages/Catalog.tsx
+++ trabajo-final/src/pages/Catalog.tsx
@@ -1,6 +1,5 @@
 import { MainLayout } from "@/layouts/MainLayout";
 import { Outlet } from "react-router-dom";
-import { Filtros } from "@/components/Filtros";
 import ListaLibros from "@/components/ListaLibros";
 
 export const Catalog: React.FC = () => {
@@ -10,7 +9,6 @@ export const Catalog: React.FC = () => {
         <h1 className="text-4xl font-bold text-center mb-8 mt-6">
           Catálogo de Libros
         </h1>
-        <Filtros />
         <Outlet />
         <ListaLibros />
       </section>
