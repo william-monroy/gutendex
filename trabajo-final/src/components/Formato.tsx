6708646e6c20e8a24b428177c56996ef49cf62d7
Santi Ayechu <santiayechu@gmail.com>
2024-05-14 18:37:32 +0200

    style: 💄 Cambiar sección de GRID a FLEX

--- trabajo-final/src/components/Formato.tsx
+++ trabajo-final/src/components/Formato.tsx
@@ -6,35 +6,24 @@ export const Formato: React.FC = () => {
     <Section isDark={false}>
       <h2 className="text-4xl font-bold">Formatos Disponibles</h2>
       <p className="text-[#4D4C4D] dark:text-[#C6C6C6]">
-        Descubre los diferentes formatos de libros disponibles en nuestra plataforma.
-        Conviértete en un lector ávido con la variedad de opciones que ofrecemos.
-      </p>  
+        Descubre los diferentes formatos de libros disponibles en nuestra
+        plataforma. Conviértete en un lector ávido con la variedad de opciones
+        que ofrecemos.
+      </p>
 
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] justify-center items-center gap-4 mt-8" style={{ gridAutoFlow: 'dense' }}>
-        <Icono
-          src="/images/undraw_contact_us_15o2.svg"
-          heading="HTML"
-        />
-        <Icono
-          src="/images/undraw_contact_us_15o2.svg"
-          heading="EPUB"
-        />
+      <div className="flex flex-row flex-wrap justify-center items-center gap-4 mt-8">
+        <Icono src="/images/undraw_contact_us_15o2.svg" heading="HTML" />
+        <Icono src="/images/undraw_contact_us_15o2.svg" heading="EPUB" />
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
           heading="Mobipocket Ebook"
         />
-        <Icono
-          src="/images/undraw_contact_us_15o2.svg"
-          heading="RDF"
-        />
+        <Icono src="/images/undraw_contact_us_15o2.svg" heading="RDF" />
         <Icono
           src="/images/undraw_contact_us_15o2.svg"
           heading="Octect Stream"
         />
-        <Icono
-          src="/images/undraw_contact_us_15o2.svg"
-          heading="Solo Texto"
-        />
+        <Icono src="/images/undraw_contact_us_15o2.svg" heading="Solo Texto" />
       </div>
     </Section>
   );
